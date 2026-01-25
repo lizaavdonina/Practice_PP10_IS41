fd5de415241e7ebf3370a5d4ba124d7d01f12152
lizaavdonina <elizavetaavdonina511@gmail.com>
2026-01-25 19:30:34 +0300

    Create Role.cs

---
+++ src/Models/Role.cs
@@ -0,0 +1,19 @@
+public class Role
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [StringLength(200)]
+        public string Description { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime CreatedDate { get; set; }
+
+        // Навигационные свойства
+        public ICollection<User> Users { get; set; }
+    }
