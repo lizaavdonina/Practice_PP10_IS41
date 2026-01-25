18dcfe1d59a4e16055f9ecf4f035a838d34a918a
lizaavdonina <elizavetaavdonina511@gmail.com>
2026-01-25 19:28:56 +0300

    Update AuditLog.cs

--- src/Models/AuditLog.cs
+++ src/Models/AuditLog.cs
@@ -1,39 +1,33 @@
-using System;
-using System.ComponentModel.DataAnnotations;
-
-namespace SiteSB.Models
-{
-    public class AuditLog
+public class AuditLog
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
-        [Display(Name = "Пользователь")]
         public int? UserId { get; set; }
 
         [Required]
-        [Display(Name = "Действие")]
-        public string ?Action { get; set; }
+        [StringLength(100)]
+        public string Action { get; set; }
 
         [Required]
-        [Display(Name = "Тип сущности")]
-        public string ?EntityType { get; set; }
+        [StringLength(50)]
+        public string EntityType { get; set; }
 
-        [Display(Name = "ID сущности")]
         public int? EntityId { get; set; }
 
-        [Display(Name = "Старые значения")]
-        public string ?OldValues { get; set; }
+        public string OldValues { get; set; }
 
-        [Display(Name = "Новые значения")]
-        public string ?NewValues { get; set; }
+        public string NewValues { get; set; }
 
-        [Display(Name = "Время")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
         public DateTime Timestamp { get; set; }
 
-        [Display(Name = "IP адрес")]
-        public string ?IPAddress { get; set; }
+        [StringLength(50)]
+        public string IPAddress { get; set; }
 
-        public virtual User ?User { get; set; }
+        // Навигационные свойства
+        [ForeignKey("UserId")]
+        public User User { get; set; }
     }
 }
