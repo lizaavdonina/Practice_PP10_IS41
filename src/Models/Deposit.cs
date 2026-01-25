63ab798d8a3d075b8567a683aaec3347da92b750
lizaavdonina <elizavetaavdonina511@gmail.com>
2026-01-25 19:39:03 +0300

    Update Deposit.cs

--- src/Models/Deposit.cs
+++ src/Models/Deposit.cs
@@ -7,7 +7,6 @@ namespace SiteSB.Models
 public class Deposit
     {
         [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
         [Required]
