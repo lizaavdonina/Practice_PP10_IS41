3082a2fcccf15af230c4f394cbaec73dfe2f4a0a
lizaavdonina <elizavetaavdonina511@gmail.com>
2026-01-25 19:40:24 +0300

    Update User.cs

--- src/Models/User.cs
+++ src/Models/User.cs
@@ -1,7 +1,12 @@
-public class User
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SiteSB.Models
+{
+    [Table("User")]
+    public class User
     {
         [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
         [Required]
