d0355cdc56e46225d0da9b1254767b3a08810983
lizaavdonina <elizavetaavdonina511@gmail.com>
2026-01-25 19:38:50 +0300

    Update DepositType.cs

--- src/Models/DepositType.cs
+++ src/Models/DepositType.cs
@@ -6,7 +6,6 @@ namespace SiteSB.Models
 public class DepositType
     {
         [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
         [Required]
